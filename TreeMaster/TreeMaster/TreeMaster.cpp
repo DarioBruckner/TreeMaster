35a029325a2e7363818fe8a58ff1f8d04e8f2a7e
Dario Bruckner <dariobruckner@gmail.com>
2021-04-12 10:48:11 +0200

    Added Main loop
    added loop in main to handle user input

--- TreeMaster/TreeMaster/TreeMaster.cpp
+++ TreeMaster/TreeMaster/TreeMaster.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <stdio.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    std::cout << "Hello Jacob!\n";
+    int input = 0; 
+    std::cout << "Willkommen im TreeMaster, hier können Sie Ihre Daten mittels \"Binärytree-Struktur\" speichern und suchen" << std::endl;
+    while(1){
+        std::cout << "Was wollen Sie machen? (0) Beendet das Program (1)..." << std::endl;
+        std::cin >> input;
+        if(input == 0){
+            break;
+        } else{
+            std::cout << "Noch in Arbeit" << std::endl;
+        }
+    }
 }
 
