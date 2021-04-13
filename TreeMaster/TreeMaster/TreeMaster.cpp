87793f5623bbdcf5b5d50b650c532a0d3e5ed69a
jacob-sch <jacob.schmauss@gmail.com>
2021-04-13 15:08:04 +0200

    implemented addnode function
    add Node function inkl. test und testautputs die noch entfernt gehören will sie aber momentan noch für nachvollziebarkeit von Rekursion

--- TreeMaster/TreeMaster/TreeMaster.cpp
+++ TreeMaster/TreeMaster/TreeMaster.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <stdio.h>
+#include "classes/TreeNode.h"
 
 int main()
 {
-    int input = 0; 
+    TreeNode* root;
+    root = new TreeNode();
+    int input = 0;
     std::cout << "Willkommen im TreeMaster, hier können Sie Ihre Daten mittels \"Binärytree-Struktur\" speichern und suchen" << std::endl;
     while(1){
         std::cout << "Was wollen Sie machen? (0) Beendet das Program (1)..." << std::endl;
@@ -11,6 +14,13 @@ int main()
         if(input == 0){
             break;
         } else{
+            //test addNode function:
+            root->addNode(5);
+            root->addNode(3);
+            root->addNode(7);
+            root->addNode(8);
+            root->addNode(17);
+            root->addNode(5);
             std::cout << "Noch in Arbeit" << std::endl;
         }
     }
