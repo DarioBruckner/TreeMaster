775acaded83c5291f3cf15dab1accec0e7c97b0a
Dario Bruckner <dariobruckner@gmail.com>
2021-04-25 20:41:31 +0200

    Revert "delete tree"

--- TreeMaster/TreeMaster/classes/TreeNode.h
+++ TreeMaster/TreeMaster/classes/TreeNode.h
@@ -24,8 +24,6 @@ class TreeNode
         int subtreeSearch(TreeNode* root, std::vector<int> subtree, int counter);
         bool checkIfContains(std::vector<int> subtree, int data);
         TreeNode* findRoot(TreeNode* root, int rootdata);
-        void deleteTree(TreeNode* root);
-
     private:
         TreeNode* left = nullptr;
         TreeNode* right = nullptr;
