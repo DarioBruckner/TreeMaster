b05b2dc25038b6ce1ef93e766ba066547cf751b6
Dario Bruckner <dariobruckner@gmail.com>
2021-04-25 20:53:36 +0200

    Update TreeNode.h

--- TreeMaster/TreeMaster/classes/TreeNode.h
+++ TreeMaster/TreeMaster/classes/TreeNode.h
@@ -27,6 +27,7 @@ class TreeNode
         int subtreeSearch(TreeNode* root, std::vector<int> subtree, int counter);
         bool checkIfContains(std::vector<int> subtree, int data);
         TreeNode* findRoot(TreeNode* root, int rootdata);
+        void deleteTree(TreeNode* root);
     private:
 
         int data = -1;
